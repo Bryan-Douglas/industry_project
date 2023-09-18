252b847a3a48e64fa83210303bda5dc5cb216d29
Fatumah Waggeh <fatumahwaggeh@gmail.com>
2023-09-18 15:20:39 -0400

    fixed alyout for scent and skin needs page

--- src/components/Scent/Scent.jsx
+++ src/components/Scent/Scent.jsx
@@ -16,51 +16,47 @@ function Scent() {
     };
   return (
     <div className="skin-section">
-    <div className="skin-section__textWrapper">
-  <h1 className="skin-section__header">
-    What are your skin needs?
-  </h1>
-  <p className="skin-section__subtitle">Choose 2 or more</p>
-  </div>
-  <div className="skin-section__cards-container">
-  
-  <div className="skin-section__card-container--top">
-  <div onClick={goBack} className="skin-section__arrow-wrapper">
+    <div onClick={goBack} className="skin-section__arrow-wrapper">
       <img src={BackArrow} alt="back arrow" />
       <p className="skin-section__arrow-text">Back</p>
     </div>
-  
-    <div className="skin-section__card">
-      <img src={Citrus} className="skin-section__image--radius"></img>
-      <p className="skin-section__subheader">Dry</p>
+    <div className="skin-section__content-wrapper">
+      <h1 className="skin-section__header">
+       What are your skin needs?
+       <p className="skin-section__subtitle">Choose 2 or more</p>
+      </h1>
+      <div className="skin-section__cards-container__needs">
+        
+          <div className="skin-section__card">
+            <img src={Citrus} className="skin-section__image--radius"></img>
+            <p className="skin-section__subheader">Citrusy</p>
+          </div>
+          <div className="skin-section__card">
+            <img src={Earthy} className="skin-section__image--radius"></img>
+            <p className="skin-section__subheader">Earthy</p>
+          </div>
+          <div className="skin-section__card">
+            <img src={Floral} className="skin-section__image--radius"></img>
+            <p className="skin-section__subheader">Floral</p>
+          </div>
+          <div className="skin-section__card">
+            <img src={Herbal} className="skin-section__image--radius"></img>
+            <p className="skin-section__subheader">Herbal</p>
+          </div>
+          <div className="skin-section__card">
+            <img src={Minty} className="skin-section__image--radius"></img>
+            <p className="skin-section__subheader">Minty</p>
+          </div>
+        
+      </div>
     </div>
-    <div className="skin-section__card">
-    <img src={Earthy} className="skin-section__image--radius"></img>
-      <p className="skin-section__subheader">Normal</p>
-    </div>
-    <div className="skin-section__card">
-    <img src={Floral} className="skin-section__image--radius"></img>
-      <p className="skin-section__subheader">Oily</p>
-    </div>
-    <Link to="/price">
-    <div  className="skin-section__arrow-wrapper">
-      <img src={NextArrow} alt="next arrow" />
-      <p className="skin-section__arrow-text">Next</p>
-    </div>
+    <Link to="/scent">
+      <div className="skin-section__arrow-wrapper">
+        <img src={NextArrow} alt="next arrow" />
+        <p className="skin-section__arrow-text">Next</p>
+      </div>
     </Link>
   </div>
-  <div className="skin-section__card-container--bottom">
-    <div className="skin-section__card">
-    <img src={Herbal} className="skin-section__image--radius"></img>
-      <p className="skin-section__subheader">Sensitive</p>
-    </div>
-    <div className="skin-section__card">
-    <img src={Minty} className="skin-section__image--radius"></img>
-      <p className="skin-section__subheader">Combination</p>
-    </div>
-  </div>
-  </div>
-</div>
   )
 }
 
--- src/components/Skin/test.jsx
+++ src/components/Skin/test.jsx
@@ -45,3 +45,50 @@
  </div>
  </Link>
 </div>
+
+{/* <div className="skin-section">
+<div className="skin-section__textWrapper">
+<h1 className="skin-section__header">
+What are your skin needs?
+</h1>
+<p className="skin-section__subtitle">Choose 2 or more</p>
+</div>
+<div className="skin-section__cards-container">
+
+<div className="skin-section__card-container--top">
+<div onClick={goBack} className="skin-section__arrow-wrapper">
+  <img src={BackArrow} alt="back arrow" />
+  <p className="skin-section__arrow-text">Back</p>
+</div>
+
+<div className="skin-section__card">
+  <img src={Acne} className="skin-section__image--radius"></img>
+  <p className="skin-section__subheader">Dry</p>
+</div>
+<div className="skin-section__card">
+<img src={Sensitive} className="skin-section__image--radius"></img>
+  <p className="skin-section__subheader">Normal</p>
+</div>
+<div className="skin-section__card">
+<img src={Dry} className="skin-section__image--radius"></img>
+  <p className="skin-section__subheader">Oily</p>
+</div>
+<Link to="/scent">
+<div  className="skin-section__arrow-wrapper">
+  <img src={NextArrow} alt="next arrow" />
+  <p className="skin-section__arrow-text">Next</p>
+</div>
+</Link>
+</div>
+<div className="skin-section__card-container--bottom">
+<div className="skin-section__card">
+<img src={Dull} className="skin-section__image--radius"></img>
+  <p className="skin-section__subheader">Sensitive</p>
+</div>
+<div className="skin-section__card">
+<img src={Texture} className="skin-section__image--radius"></img>
+  <p className="skin-section__subheader">Combination</p>
+</div>
+</div>
+</div>
+</div> */}
